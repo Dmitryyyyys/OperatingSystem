ab49abbb74ce3399334522ecc02bf3cb77b08031
Dmitryyyyys <118775938+Dmitryyyyys@users.noreply.github.com>
2025-03-15 02:40:17 +0300

    add lab5

---
+++ lab5/OS05_01/OS05_01/OS05_01.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <bitset>
+
+int main() {
+    // Получение идентификатора текущего процесса
+    DWORD processId = GetCurrentProcessId();
+    std::cout << "Process ID: " << processId << std::endl;
+
+    // Получение идентификатора текущего потока (main thread)
+    DWORD threadId = GetCurrentThreadId();
+    std::cout << "Thread ID: " << threadId << std::endl;
+
+    // Получение приоритетного класса процесса
+    HANDLE hProcess = GetCurrentProcess();
+    DWORD processPriorityClass = GetPriorityClass(hProcess);
+    std::cout << "Process Priority Class: " << processPriorityClass << std::endl;
+
+    // Получение приоритета текущего потока
+    HANDLE hThread = GetCurrentThread();
+    int threadPriority = GetThreadPriority(hThread);
+    std::cout << "Thread Priority: " << threadPriority << std::endl;
+
+    // Получение маски доступных процессору процессоров (affinity mask)
+    DWORD_PTR processAffinityMask, systemAffinityMask;
+    if (GetProcessAffinityMask(hProcess, &processAffinityMask, &systemAffinityMask)) {
+        std::cout << "Process Affinity Mask (binary): " << std::bitset<64>(processAffinityMask) << std::endl;
+    }
+    else {
+        std::cerr << "Failed to get affinity mask" << std::endl;
+    }
+
+    // Количество доступных процессоров
+    SYSTEM_INFO sysInfo;
+    GetSystemInfo(&sysInfo);
+    std::cout << "Number of Processors: " << sysInfo.dwNumberOfProcessors << std::endl;
+
+    // Получение номера процессора, на котором выполняется текущий поток
+    DWORD processorNumber = GetCurrentProcessorNumber();
+    std::cout << "Current Processor: " << processorNumber << std::endl;
+
+    return 0;
+}
---
+++ lab5/OS05_03/OS05_03/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+class Program
+{
+    const int ThreadCount = 10;           // Количество потоков
+    const int ThreadLifeTime = 10;        // Время жизни потока
+    const int ObservationTime = 30;       // Время наблюдения
+    static int[,] Matrix = new int[ThreadCount, ObservationTime];
+    static DateTime StartTime = DateTime.Now;
+
+    static void WorkThread(object o)
+    {
+        int id = (int)o;
+        for (int i = 0; i < ThreadLifeTime * 20; i++)
+        {
+            DateTime CurrentTime = DateTime.Now;
+            int ElapsedSeconds = (int)Math.Round(CurrentTime.Subtract(StartTime).TotalSeconds - 0.49);
+
+            if (ElapsedSeconds >= 0 && ElapsedSeconds < ObservationTime)
+            {
+                Matrix[id, ElapsedSeconds] += 50;
+            }
+
+            MySleep(50);
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        Process.GetCurrentProcess().ProcessorAffinity = (System.IntPtr)15;  // Ограничение по процессорам (для 4 логических процессоров)
+        Thread[] t = new Thread[ThreadCount];
+
+        for (int i = 0; i < ThreadCount; ++i)
+        {
+            object o = i;
+            t[i] = new Thread(WorkThread);
+
+            // Устанавливаем приоритеты для потоков
+            if (i < ThreadCount / 2) // Половина потоков с наименьшим приоритетом
+            {
+                t[i].Priority = ThreadPriority.Lowest;
+            }
+            else // Остальные потоки с наибольшим приоритетом
+            {
+                t[i].Priority = ThreadPriority.Highest;
+            }
+
+            t[i].Start(o);
+        }
+
+        Console.WriteLine("A student ... is waiting for the threads to finish");
+
+        for (int i = 0; i < ThreadCount; ++i)
+        {
+            t[i].Join();
+        }
+
+        // Вывод результатов
+        for (int s = 0; s < ObservationTime; s++)
+        {
+            Console.Write("{0,3}: ", s);
+            for (int th = 0; th < ThreadCount; th++)
+            {
+                Console.Write(" T{0}:{1,5}", th, Matrix[th, s]);
+            }
+            Console.WriteLine();
+        }
+
+    }
+
+    static void MySleep(int ms)
+    {
+        Stopwatch stopwatch = Stopwatch.StartNew();
+        double sum = 0;
+
+        while (stopwatch.ElapsedMilliseconds < ms)
+        {
+            for (int k = 0; k < 1000; k++)
+            {
+                sum += Math.Sqrt(k) * Math.Cos(k);
+            }
+        }
+    }
+}
